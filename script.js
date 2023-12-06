9b5e0168215c8c26bd5b67266d92cdb13b5c8dff
xyssatu <149461881+xyssatu@users.noreply.github.com>
2023-12-06 23:14:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ async function init1() {
   player.configure({
     drm: {
       clearKeys: {
-        '9ba3e153ef8956d6e2b0684fcf74f58f': 'dbc28cb5c6426080f984a5b6d436bb30'
+        '45fec91ce1f19b6b1f31d69dcfaaf6cd': '843e228ab109e9aa6c4822ee4ad05d7d'
       }
     }
   });
@@ -70,7 +70,7 @@ async function init2() {
   player.configure({
     drm: {
       clearKeys: {
-        'fadd2720deff5925ab86df0819cd7775': 'f67ff08c7ebc779f6a6fcfc83835f65b'
+        '4d38060bf41b3c29df0ec950ece6b5da': '7ee9506b13480491d79b71c062ab5366'
       }
     }
   });
@@ -213,7 +213,7 @@ async function init6() {
   player.configure({
     drm: {
       clearKeys: {
-        'fadd2720deff5925ab86df0819cd7775': 'f67ff08c7ebc779f6a6fcfc83835f65b'
+        '57d2ac9210cfbca3596cc679a01c8b29': 'd5e35c0f39c76adf24853d7ea18c71e7'
       }
     }
   });
@@ -248,7 +248,7 @@ async function init7() {
   player.configure({
     drm: {
       clearKeys: {
-        'fadd2720deff5925ab86df0819cd7775': 'f67ff08c7ebc779f6a6fcfc83835f65b'
+        '4d38060bf41b3c29df0ec950ece6b5da': '7ee9506b13480491d79b71c062ab5366'
       }
     }
   });
@@ -283,7 +283,7 @@ async function init8() {
   player.configure({
     drm: {
       clearKeys: {
-        'fadd2720deff5925ab86df0819cd7775': 'f67ff08c7ebc779f6a6fcfc83835f65b'
+        'd2159ffe3be22ed4916a6abe4d58d265': '3e4efcec08d5d5c18a403b7048a43638'
       }
     }
   });
